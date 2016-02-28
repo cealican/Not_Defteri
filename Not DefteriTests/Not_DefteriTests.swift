6c398e82151c571c6e78cd16e06e638b1ff2dcb2
Ali Can <alican@ali-macbook-air.local>
2016-02-28 13:14:14 +0200

    Initial Commit

---
+++ Not DefteriTests/Not_DefteriTests.swift
@@ -0,0 +1,36 @@
+//
+//  Not_DefteriTests.swift
+//  Not DefteriTests
+//
+//  Created by Ali Can on 28.02.2016.
+//  Copyright © 2016 Ali Can. All rights reserved.
+//
+
+import XCTest
+@testable import Not_Defteri
+
+class Not_DefteriTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
